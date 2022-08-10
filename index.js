936dea00527a0de12076bbeb851e3d346d914eaf
馬儀鈞 <frela0907@gmail.com>
2022-08-10 22:51:21 +0800

    兩頁觸發事件

--- index.js
+++ index.js
@@ -2,3 +2,32 @@ $(window).load(function () { // 確認整個頁面讀取完畢再將這三個div
     $("#status").delay(2000).fadeOut(2000); //delay --> 延遲幾秒才fadeOut
     $("#preloader").delay(2000).fadeOut(2000);
 })
+
+//Bootstrap 模板
+
+let LeftPage = document.querySelector(".leftPage");
+let RightPage = document.querySelector(".rightPage");
+
+//初始化
+//LeftPage 將資料搬到ＪＳ中用inner html放入
+
+// {
+//     let leftPageHTML = ``;
+//     LeftPage.innerHTML = leftPageHTML;
+// }
+
+LeftPage.addEventListener("mouseover", function (event) {
+    LeftPage.style.zIndex = 10;
+})
+
+RightPage.addEventListener("mouseover", function (event) {
+    RightPage.style.zIndex = 10;
+})
+
+LeftPage.addEventListener("mouseout", function (event) {
+    LeftPage.style.zIndex = 0;
+})
+
+RightPage.addEventListener("mouseout", function (event) {
+    RightPage.style.zIndex = 0;
+})
